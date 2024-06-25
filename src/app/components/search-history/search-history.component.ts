21fff7a9482ccd188154a64d86fde1dcf7fb8447
Thiago <tbrm.bit@gmail.com>
2024-06-25 14:46:51 -0300

    Created app and components

---
+++ src/app/components/search-history/search-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-history.component.html',
+  styleUrl: './search-history.component.scss'
+})
+export class SearchHistoryComponent {
+
+}
